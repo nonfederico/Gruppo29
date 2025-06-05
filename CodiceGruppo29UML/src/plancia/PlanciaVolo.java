f91571f061f611c8810b8f9f396fcd5d4a088fef
reprogrammato <riccardoworkspace@gmail.com>
2025-06-05 21:17:07 +0200

    Classe PlanciaVolo

--- CodiceGruppo29UML/src/plancia/PlanciaVolo.java
+++ CodiceGruppo29UML/src/plancia/PlanciaVolo.java
@@ -2,17 +2,100 @@ package plancia;
 
 import java.io.*;
 import java.util.*;
+import player.Giocatore;
 
 /**
  * 
  */
 public class PlanciaVolo {
 
-    /**
-     * Default constructor
-     */
+	private final int LUNGHEZZA = 21;
+	private ArrayList[] percorso;
+	
     public PlanciaVolo() {
+    	
+    	percorso = new ArrayList[LUNGHEZZA];
+    	
+    	for(int i = 0; i < LUNGHEZZA; i++ ) {
+    		
+    		percorso[i] = new ArrayList();
+    	}
+    	
     }
+    
+//    aggiuge un giocatore nella posizione 0 (iniziale)
+    
+    public void aggiungiGiocatore(Giocatore g) {
+    	
+    	percorso[0].add(g);
+    }
+    
+//    Trova la posizione di un determinato giocatore
+    
+    public int trovaPosizione(Giocatore g) {
+    	
+    	for(int i = 0; i < LUNGHEZZA; i++) {
+    		
+    		if(percorso[i].contains(g)) {
+    			
+    			return i;
+    		}
+    	}
+    	
+    	return -1;
+    }
+    
+//    Muove un giocatore di un certo numero di passi
+    
+    public void muoviGiocatore(Giocatore g, int passi) {
+    	
+    	int posizioneCorrente = trovaPosizione(g);
+    	if(posizioneCorrente == -1) {
+    		
+    		return;
+    	}
+    	
+    	percorso[posizioneCorrente].remove(g);
+    	int nuovaPosizione = Math.min(posizioneCorrente + passi, LUNGHEZZA -1);
+    	percorso[nuovaPosizione].add(g);
+    	
+    }
+    
+    
+//    determina chi è più avanti nel tracciato 
+    
+    public Giocatore getLeader() {
+    	
+    	for(int i = LUNGHEZZA - 1; i >= 0; i-- ) {
+    		
+    		if(!percorso[i].isEmpty()) {
+    			
+    			return percorso[i].get(0); // giocatore nella posizione più avanzata
+    		}
+    	}
+    	
+    	return null;
+    }
+    
+//    visualizza la plancia di volo 
+    
+    public void stampaPlancia() {
+    	
+    	for(int i = 0; i < LUNGHEZZA ; i++) {
+    		
+    		System.out.println("[ " + i + " ]");
+    		for(Giocatore g : percorso[i]) {
+    			
+    			System.out.println(g.getNome() + " ");
+    			
+    		}
+    		
+    		System.out.println();
+    	}
+    }
+    
+    
+    
 
 
     /**
--- CodiceGruppo29UML/src/player/MatricePosizione.java
+++ CodiceGruppo29UML/src/player/MatricePosizione.java
@@ -12,6 +12,7 @@ public class MatricePosizione {
      * Default constructor
      */
     public MatricePosizione() {
+    	
     }
 
 
