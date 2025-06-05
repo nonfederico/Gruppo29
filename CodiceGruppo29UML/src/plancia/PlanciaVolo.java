ffdedab5ea2bc60f71fe1f0c3429166f1088d0b0
reprogrammato <riccardoworkspace@gmail.com>
2025-06-05 21:18:14 +0200

    Update PlanciaVolo.java

--- CodiceGruppo29UML/src/plancia/PlanciaVolo.java
+++ CodiceGruppo29UML/src/plancia/PlanciaVolo.java
@@ -94,76 +94,76 @@ public class PlanciaVolo {
     	}
     }
     
-    
-    
-
-
-    /**
-     * 
-     */
-    public int[] posizioneGiocatori;
-
-    /**
-     * 
-     */
-    public int crediti;
-
-
-
-
-
-    /**
-     * 
-     */
-    public void visualizzaPericoli() {
-        // TODO implement here
-    }
-
-    /**
-     * richiamata a fine di ogni turno
-     */
-    public void ritornaOrdineGiocatori() {
-        // TODO implement here
-    }
-
-    /**
-     * 
-     */
-    public void spostaGiocatori() {
-        // TODO implement here
-    }
-
-    /**
-     * 
-     */
-    public void ricompensaOrdineArrivo() {
-        // TODO implement here
-    }
-
-    /**
-     * 
-     */
-    public void ricompensaNaveBella() {
-        // TODO implement here
-    }
-
-    /**
-     * 
-     */
-    public void ricompensaMerci() {
-        // TODO implement here
-    }
-
-    /**
-     * 
-     */
-    public void penalitàPerdite() {
-        // TODO implement here
-    }
-
-}
-
-enum tipologia {
-	Livello0,
-	Livello1
-}
+//    
+//    
+//
+//
+//    /**
+//     * 
+//     */
+//    public int[] posizioneGiocatori;
+//
+//    /**
+//     * 
+//     */
+//    public int crediti;
+//
+//
+//
+//
+//
+//    /**
+//     * 
+//     */
+//    public void visualizzaPericoli() {
+//        // TODO implement here
+//    }
+//
+//    /**
+//     * richiamata a fine di ogni turno
+//     */
+//    public void ritornaOrdineGiocatori() {
+//        // TODO implement here
+//    }
+//
+//    /**
+//     * 
+//     */
+//    public void spostaGiocatori() {
+//        // TODO implement here
+//    }
+//
+//    /**
+//     * 
+//     */
+//    public void ricompensaOrdineArrivo() {
+//        // TODO implement here
+//    }
+//
+//    /**
+//     * 
+//     */
+//    public void ricompensaNaveBella() {
+//        // TODO implement here
+//    }
+//
+//    /**
+//     * 
+//     */
+//    public void ricompensaMerci() {
+//        // TODO implement here
+//    }
+//
+//    /**
+//     * 
+//     */
+//    public void penalitàPerdite() {
+//        // TODO implement here
+//    }
+//
+//}
+//
+//enum tipologia {
+//	Livello0,
+//	Livello1
+//}
