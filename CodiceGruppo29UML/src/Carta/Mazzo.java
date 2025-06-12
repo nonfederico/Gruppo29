790a2f18e81ba17f7473a2544279632aab8279b8
nonfederico <fedecattaneo@outlook.com>
2025-06-12 04:27:37 +0200

    modifiche

--- CodiceGruppo29UML/src/Carta/Mazzo.java
+++ CodiceGruppo29UML/src/Carta/Mazzo.java
@@ -4,49 +4,41 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Stack;
 
-import javax.swing.SwingWorker;
+public class Mazzo {
 
-  public class Mazzo  {
-	
-	 private Mazzo istanza = null; 
-	  
-	private Mazzo () {
-		
+	private Mazzo istanza = null;
+
+	private Mazzo() {
+
 	}
 
-	
-	public Mazzo getMazzo () {
-		
-		if (istanza==null) {
-			
+	public Mazzo getMazzo() {
+
+		if (istanza == null) {
+
 			istanza = new Mazzo();
-			
-		} return istanza;
-		
-		
-	}
-	
-	private Stack<Carte> mazzo = new Stack<Carte>() ;
-  
-	  public Stack<Carte> creaMazzoMescolato() {
-	        ArrayList<Carte> carte = Carte.getListaCarte(); // chiamata statica
-	        Collections.shuffle(carte);
-	       
-	        for (int i = 0 ; i <8 ; i++) {
-	            mazzo.push(carte.get(i));
-	        }
 
-	        return mazzo;
-	    }
+		}
+		return istanza;
+
+	}
+
+	private Stack<Carte> mazzo = new Stack<Carte>();
+
+	public Stack<Carte> creaMazzoMescolato() {
+		ArrayList<Carte> carte = Carte.getListaCarte(); // chiamata statica
+		Collections.shuffle(carte);
+
+		for (int i = 0; i < 8; i++) {
+			mazzo.push(carte.get(i));
+		}
+
+		return mazzo;
+	}
 
 	public Carte getCarta() {
-		
-			return mazzo.pop();
+		return mazzo.pop();
 
 	}
-	
-
-
-
 
 }
